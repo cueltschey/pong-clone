acff9236b69f36c87daf99082e9d5de982a85875
cueltschey <chaseuelt@gmail.com>
2023-12-05 12:25:50 -0600

    create render_state struct

--- pong-clone/render.cpp
+++ pong-clone/render.cpp
@@ -1,37 +1,35 @@
 internal void
-render_pixels(void* buffer_memory, int h, int w) {
-	unsigned int* pixel = (unsigned int*)buffer_memory;
-	for (int i = 0; i < h; ++i) {
-		for (int j = 0; j < w; ++j) {
+render_pixels() {
+	unsigned int* pixel = (unsigned int*)renderstate.buffer_memory;
+	for (int i = 0; i < renderstate.height; ++i) {
+		for (int j = 0; j < renderstate.width; ++j) {
 			*pixel++ = i * 0xfff + j * 0xffEE00;
 		}
 	}
 }
 internal void
-clear_screen(void* buffer_memory, int h, int w, unsigned int color) {
-	unsigned int* pixel = (unsigned int*)buffer_memory;
-	for (int i = 0; i < h; ++i) {
-		for (int j = 0; j < w; ++j) {
+clear_screen(unsigned int color) {
+	unsigned int* pixel = (unsigned int*)renderstate.buffer_memory;
+	for (int i = 0; i < renderstate.height; ++i) {
+		for (int j = 0; j < renderstate.width; ++j) {
 			*pixel++ = color;
 		}
 	}
 }
 
 internal void
-draw_rect_in_pixels(void* buffer_memory, int w, int h, int x0, int y0, int x1, int y1, unsigned int color) {
+draw_rect_in_pixels(int x0, int y0, int x1, int y1, unsigned int color) {
 
-	x0 = clamp(0, x0, w);
-	x1 = clamp(0, x1, w);
-	y0 = clamp(0, y0, h);
-	y1 = clamp(0, y1, h);
+	x0 = clamp(0, x0, renderstate.width);
+	x1 = clamp(0, x1, renderstate.width);
+	y0 = clamp(0, y0, renderstate.height);
+	y1 = clamp(0, y1, renderstate.height);
 
 
 	for (int y = y0; y < y1; ++y) {
-		unsigned int* pixel = (unsigned int*)buffer_memory + x0 + y * w;
+		unsigned int* pixel = (unsigned int*)renderstate.buffer_memory + x0 + y * renderstate.width;
 		for (int x = x0; x < x1; x++) {
 			*pixel++ = color;
 		}
 	}
 }
-
-draw_rect(){}
--- pong-clone/win_32_platform.cpp
+++ pong-clone/win_32_platform.cpp
@@ -1,12 +1,16 @@
 #include <windows.h>
 #include "utils.cpp"
-#include "render.cpp"
 
 global_var bool running = true;
-global_var int w;
-global_var int h;
-global_var  void* buffer_memory;
-BITMAPINFO	buffer_bitmap_info;
+
+struct Render_State {
+	int height, width;
+	void* buffer_memory;
+	BITMAPINFO	buffer_bitmap_info;
+};
+global_var Render_State renderstate;
+#include "render.cpp"
+
 
 LRESULT CALLBACK window_callback(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
 	LRESULT result = 0;
@@ -21,19 +25,19 @@ LRESULT CALLBACK window_callback(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lPa
 		case WM_SIZE: {
 			RECT rect;
 			GetClientRect(hwnd, &rect);
-			w = rect.right - rect.left;
-			h = rect.bottom - rect.top;
+			renderstate.width = rect.right - rect.left;
+			renderstate.height = rect.bottom - rect.top;
 
-			int buffer_size = w * h * sizeof(unsigned int);
-			if (buffer_memory) VirtualFree(buffer_memory, 0, MEM_RELEASE);
-			buffer_memory = VirtualAlloc(0, buffer_size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+			int buffer_size = renderstate.width * renderstate.height * sizeof(unsigned int);
+			if (renderstate.buffer_memory) VirtualFree(renderstate.buffer_memory, 0, MEM_RELEASE);
+			renderstate.buffer_memory = VirtualAlloc(0, buffer_size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
 
-			buffer_bitmap_info.bmiHeader.biSize = sizeof(buffer_bitmap_info.bmiHeader);
-			buffer_bitmap_info.bmiHeader.biWidth = w;
-			buffer_bitmap_info.bmiHeader.biHeight = h;
-			buffer_bitmap_info.bmiHeader.biPlanes = 1;
-			buffer_bitmap_info.bmiHeader.biBitCount = 32;
-			buffer_bitmap_info.bmiHeader.biCompression = BI_RGB;
+			renderstate.buffer_bitmap_info.bmiHeader.biSize = sizeof(renderstate.buffer_bitmap_info.bmiHeader);
+			renderstate.buffer_bitmap_info.bmiHeader.biWidth = renderstate.width;
+			renderstate.buffer_bitmap_info.bmiHeader.biHeight = renderstate.height;
+			renderstate.buffer_bitmap_info.bmiHeader.biPlanes = 1;
+			renderstate.buffer_bitmap_info.bmiHeader.biBitCount = 32;
+			renderstate.buffer_bitmap_info.bmiHeader.biCompression = BI_RGB;
 		} break;
 
 		default: {
@@ -68,8 +72,7 @@ int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int n
 			TranslateMessage(&message);
 			DispatchMessage(&message);
 		}
-		clear_screen(buffer_memory, h, w, 0xff5500);
-		draw_rect(buffer_memory, w, h, 50, 50, 200, 500, 0x00ff22);
-		StretchDIBits(hdc, 0, 0, w, h, 0, 0, w, h, buffer_memory, &buffer_bitmap_info, DIB_RGB_COLORS, SRCCOPY);
+		clear_screen(0xff5500);
+		StretchDIBits(hdc, 0, 0, renderstate.width, renderstate.height, 0, 0, renderstate.width, renderstate.height, renderstate.buffer_memory, &renderstate.buffer_bitmap_info, DIB_RGB_COLORS, SRCCOPY);
 	}
 }
