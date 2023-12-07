6d2a2633dc7bfc715bc09d5a7829d4de20dcc1d7
cueltschey <chaseuelt@gmail.com>
2023-12-07 13:25:30 -0600

    cleanup code

--- pong-clone/game.cpp
+++ pong-clone/game.cpp
@@ -8,7 +8,25 @@ float arena_half_size_x = 85, arena_half_size_y = 45;
 float player_half_size_x = 2.5, player_half_size_y = 12;
 float ball_p_x, ball_p_y, ball_dp_x = 100, ball_dp_y = 0, ball_half_size = 1;
 
-static void simulate_game(Input* input, float dt) {
+internal void
+simulate_player(float &p, float &dp, float &ddp, float dt) {
+	ddp -= dp * 10.f;
+
+	p = p + dp * dt + ddp * dt * dt * .5f;
+	dp = dp + ddp * dt;
+
+	if (p + player_half_size_y > arena_half_size_y) {
+		p = arena_half_size_y - player_half_size_y;
+		dp = 0;
+	}
+	else if (p - player_half_size_y < -arena_half_size_y) {
+		p = -arena_half_size_y + player_half_size_y;
+		dp = 0;
+	}
+}
+
+static void 
+simulate_game(Input* input, float dt) {
 	clear_screen(0xff5500);
 	draw_rect(0, 0, 85, 45, 0xffaa33);
 
@@ -20,32 +38,8 @@ static void simulate_game(Input* input, float dt) {
 	if (is_down(BUTTON_W)) player_2_ddp += 2000;
 	if (is_down(BUTTON_S)) player_2_ddp -= 2000;
 
-	player_1_ddp -= player_1_dp * 10.f;
-	player_2_ddp -= player_2_dp * 10.f;
-
-	player_1_p = player_1_p + player_1_dp * dt + player_1_ddp * dt * dt * .5f;
-	player_1_dp = player_1_dp + player_1_ddp * dt;
-
-	if (player_1_p + player_half_size_y > arena_half_size_y) {
-		player_1_p = arena_half_size_y - player_half_size_y;
-		player_1_dp = 0;
-	}
-	else if (player_1_p - player_half_size_y < -arena_half_size_y) {
-		player_1_p = -arena_half_size_y + player_half_size_y;
-		player_1_dp = 0;
-	}
-
-	player_2_p = player_2_p + player_2_dp * dt + player_2_ddp * dt * dt * .5f;
-	player_2_dp = player_2_dp + player_2_ddp * dt;
-
-	if (player_2_p + player_half_size_y > arena_half_size_y) {
-		player_2_p = arena_half_size_y - player_half_size_y;
-		player_2_dp = 0;
-	}
-	else if (player_2_p - player_half_size_y < -arena_half_size_y) {
-		player_2_p = -arena_half_size_y + player_half_size_y;
-		player_2_dp = 0;
-	}
+	simulate_player(player_1_p, player_1_dp, player_1_ddp, dt);
+	simulate_player(player_2_p, player_2_dp, player_2_ddp, dt);
 
 	ball_p_x += ball_dp_x * dt;
 	ball_p_y += ball_dp_y * dt;
@@ -75,6 +69,11 @@ static void simulate_game(Input* input, float dt) {
 	else if (ball_p_y - ball_half_size < -arena_half_size_y) {
 		ball_dp_y *= -1;
 		ball_p_y = -arena_half_size_y + ball_half_size;
+	}
+
+	if (ball_p_x + ball_half_size > arena_half_size_x || ball_p_x - ball_half_size < -arena_half_size_x) {
+		ball_dp_x *= -1;
+		ball_dp_y = 0; ball_p_x = 0; ball_p_y = 0;
 	}
 
 	draw_rect(80, player_1_p, player_half_size_x, player_half_size_y, 0xff0000);
