e3857ea0918f9a86c5c4b74db126a992ca20edee
cueltschey <chaseuelt@gmail.com>
2023-12-08 21:16:23 -0600

    added score and color change

--- pong-clone/game.cpp
+++ pong-clone/game.cpp
@@ -7,6 +7,7 @@ float player_1_p, player_1_dp, player_2_p, player_2_dp;
 float arena_half_size_x = 85, arena_half_size_y = 45;
 float player_half_size_x = 2.5, player_half_size_y = 12;
 float ball_p_x, ball_p_y, ball_dp_x = 100, ball_dp_y = 0, ball_half_size = 1;
+int score_p_1 = 0, score_p_2 = 0;
 
 internal void
 simulate_player(float &p, float &dp, float &ddp, float dt) {
@@ -27,8 +28,8 @@ simulate_player(float &p, float &dp, float &ddp, float dt) {
 
 static void 
 simulate_game(Input* input, float dt) {
-	clear_screen(0xff5500);
-	draw_rect(0, 0, 85, 45, 0xffaa33);
+	clear_screen(0x00000);
+	draw_rect(0, 0, 85, 45, 0x343434);
 
 	float player_1_ddp = 0.f;
 	if (is_down(BUTTON_UP)) player_1_ddp += 2000;
@@ -43,6 +44,7 @@ simulate_game(Input* input, float dt) {
 
 	ball_p_x += ball_dp_x * dt;
 	ball_p_y += ball_dp_y * dt;
+	ball_dp_x *= 1.0003;
 
 	draw_rect(ball_p_x, ball_p_y, ball_half_size, ball_half_size, 0xffffff);
 
@@ -52,14 +54,14 @@ simulate_game(Input* input, float dt) {
 		ball_p_y - ball_half_size < player_1_p + player_half_size_y) {
 		ball_p_x = 80 - player_half_size_x - ball_half_size;
 		ball_dp_x *= -1;
-		ball_dp_y = (ball_p_y - player_1_p) * 3;
+		ball_dp_y = player_1_dp * 0.75f;
 	}else if (ball_p_x + ball_half_size >- 80 - player_half_size_x &&
 		ball_p_x - ball_half_size < -80 + player_half_size_x &&
 		ball_p_y + ball_half_size > player_2_p - player_half_size_y &&
 		ball_p_y + ball_half_size < player_2_p + player_half_size_y) {
 		ball_p_x = -80 + player_half_size_x + ball_half_size;
 		ball_dp_x *= -1;
-		ball_dp_y = (ball_p_y - player_2_p) * 3;
+		ball_dp_y = player_2_dp * 0.75f;
 	}
 
 	if (ball_p_y + ball_half_size > arena_half_size_y) {
@@ -71,11 +73,28 @@ simulate_game(Input* input, float dt) {
 		ball_p_y = -arena_half_size_y + ball_half_size;
 	}
 
-	if (ball_p_x + ball_half_size > arena_half_size_x || ball_p_x - ball_half_size < -arena_half_size_x) {
+	if (ball_p_x + ball_half_size > arena_half_size_x) {
 		ball_dp_x *= -1;
 		ball_dp_y = 0; ball_p_x = 0; ball_p_y = 0;
+		score_p_2++;
+		ball_dp_x = 100;
+	}
+	else if (ball_p_x - ball_half_size < -arena_half_size_x) {
+		ball_dp_x *= -1;
+		ball_dp_y = 0; ball_p_x = 0; ball_p_y = 0;
+		score_p_1++;
+		ball_dp_x = 100;
 	}
 
-	draw_rect(80, player_1_p, player_half_size_x, player_half_size_y, 0xff0000);
-	draw_rect(-80, player_2_p, player_half_size_x, player_half_size_y, 0xff0000);
+	draw_rect(80, player_1_p, player_half_size_x, player_half_size_y, 0xffffff);
+	draw_rect(-80, player_2_p, player_half_size_x, player_half_size_y, 0xffffff);
+
+	if (score_p_1 >= 10 || score_p_2 >= 10) running = false;
+
+	for (int i = 0; i < score_p_1; ++i) {
+		draw_rect(80 - (i * 5), 47, 1, 1, 0xffffff);
+	}
+	for (int j = 0; j < score_p_2; ++j) {
+		draw_rect(-80 + (j * 5), 47, 1, 1, 0xffffff);
+	}
 }
